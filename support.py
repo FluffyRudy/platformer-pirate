8cd6a22bde45acf46b842f8145c82decf1fefabc
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-05-14 09:35:03 +0545

    Add support file to import folder images

---
+++ support.py
@@ -0,0 +1,13 @@
+import os
+import pygame
+
+def import_folder(path):
+    surf_list = []
+
+    sorted_files = sorted(os.listdir(path))
+    for file in sorted_files:
+        image_path = os.path.join(os.path.abspath(path), file)
+        image_surf = pygame.image.load(image_path).convert_alpha()
+        surf_list.append(image_surf)
+
+    return surf_list
