deb062c4538adad97ff12d1ba606f596336e35ca
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-05-15 20:21:39 +0545

    .

--- level/barrel.tsx
+++ level/barrel.tsx
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="barrel" tilewidth="26" tileheight="30" tilecount="1" columns="0">
+<tileset version="1.10" tiledversion="1.10.2" name="barrel" tilewidth="1200" tileheight="800" tilecount="1" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image width="26" height="30" source="../.PirateGame/Assets/Merchant Ship/Sprites/Barrel/Idle/1.png"/>
+ <tile id="2">
+  <image width="1200" height="800" source="../graphics/barrel/1.png"/>
  </tile>
 </tileset>
